caef7fa53d690bb12c165ba6113d8bde3d889f03
Yanghr <825127440@qq.com>
2025-08-26 10:04:11 +0800

    test push

--- src/App.vue
+++ src/App.vue
@@ -100,7 +100,6 @@ function updateTodo({ id, title }: ToDo) {
 
       <ul>
         <TodoItem v-for="todo in filterDataTodo" :key="todo.id" :todo="todo" @delete="deleteTodo" @toggle="toggleTodo"
-        
           @updateTodo="updateTodo" />
       </ul>
       <div class="">
